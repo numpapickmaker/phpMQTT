9ab0fde2fc244646c5876afee90135d89acdad87
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-02-05 22:36:01 +0700

    Update subscribe.php

--- examples/subscribe.php
+++ examples/subscribe.php
@@ -3,10 +3,10 @@
 require("../phpMQTT.php");
 
 
-$server = "mqtt.example.com";     // change if necessary
-$port = 1883;                     // change if necessary
-$username = "";                   // set your username
-$password = "";                   // set your password
+$server = "m13.cloudmqtt.com";     // change if necessary
+$port = 18190;                     // change if necessary
+$username = "hcnzqziw";                   // set your username
+$password = "4W-4RJEmz5Ih";                   // set your password
 $client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $mqtt = new phpMQTT($server, $port, $client_id);
